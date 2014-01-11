0ae890790c825b0a56ef36a9c1f3b90143a2074f
Sergej Orlov <wladimirych@gmail.com>
2014-01-11 14:50:55 +0400

    reverse all lines with dir=2

--- basic.sql
+++ basic.sql
@@ -1,5 +1,5 @@
 UPDATE map_podm SET label = NULL WHERE label = ' ';
 CREATE VIEW points AS SELECT * FROM map_podm WHERE GeometryType(geom)='POINT';
-CREATE VIEW lines AS SELECT * FROM map_podm WHERE GeometryType(geom)='LINESTRING';
+CREATE VIEW lines AS SELECT id, CASE WHEN dir=2 THEN ST_Reverse(geom) ELSE geom END, label, type FROM map_podm WHERE GeometryType(geom)='LINESTRING';
 CREATE VIEW polygons AS SELECT * FROM map_podm WHERE GeometryType(geom)='POLYGON';
 CREATE LANGUAGE plpythonu;
